9fa74a47885a6e0b18250ea4c0c998257fbdb26a
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2025-05-05 09:19:14 -0400

    MAP 281: Feature/ol map provider upgrade merge into develop (#113)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    This is the final step merging the map provider migration changes back
    to stable develop branch. <!--- Describe your changes in detail -->
    
    Moved javascripts related changes into the new set of JS files. The
    moved changes are from below PR:
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/110 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/109 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/102/files 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/103 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/101 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/96/files 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/87 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/84/files 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/83/files 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/105/files 
    https://github.com/usdot-fhwa-stol/connectedvcs-tools/pull/91/files 
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    https://usdot-carma.atlassian.net/browse/MAP-281
    <!-- e.g. CAR-123 -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. --> <!---
    Include details of your testing environment, and the tests you ran to
    -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [x] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    <!-- Thanks for the contribution, this is awesome. -->

---
+++ fedgov-cv-ISDcreator-webapp/src/main/webapp/js/style.js
@@ -0,0 +1,232 @@
+const laneDefault = {
+  // strokeColor: "${getStrokeColor}",
+  strokeColor: "#FF9900",
+  // fillColor: "${getFillColor}",
+  fillColor: "#FF9900",
+  strokeOpacity: 1,
+  strokeWidth: 5,
+  fillOpacity: .9,
+  pointRadius: 8,
+  label: "${getLabel}",
+  fontFamily: "Arial",
+  fontSize: "10px",
+  cursor: "pointer"
+};
+
+const measureDefault = {
+  strokeColor: "#FF9900",
+  fillColor: "#FF9900",
+  strokeOpacity: 1,
+  strokeWidth: 4,
+  fillOpacity: .9,
+  fontFamily: "Arial",
+  fontSize: "10px",
+  cursor: "pointer"
+};
+
+const barDefault = {
+  strokeColor: "#FF0000",
+  fillColor: "#FF0000",
+  strokeOpacity: 1,
+  strokeWidth: 4,
+  fillOpacity: 0,
+  pointRadius: 3
+};
+
+const vectorDefault = {
+  strokeColor: "#FF9900",
+  fillColor: "#FF9900",
+  strokeOpacity: 1,
+  strokeWidth: 1,
+  fillOpacity: 0,
+  pointRadius: 1
+};
+
+const widthDefault = {
+  strokeColor: "#FFFF00",
+  fillColor: "#FFFF00",
+  strokeOpacity: .5,
+  strokeWidth: 1,
+  fillOpacity: .1,
+  pointRadius: 1
+};
+
+const connectionsDefault = {
+  strokeColor: "#0000FF",
+  fillColor: "#0000FF",
+  strokeOpacity: 1,
+  strokeWidth: 1,
+  fillOpacity:.5,
+  pointRadius: 6,
+  graphicName: "triangle",
+  rotation: "${angle}"
+};
+
+
+const laneStyle = (feature) => {
+  return new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: feature.get('computed') ? '#FF5000' : (feature.get('source') ? '#00EDFF' : "#FF9900"),
+      width: laneDefault.strokeWidth,
+      opacity: laneDefault.strokeOpacity
+    }),
+    fill: new ol.style.Fill({
+      color: feature.get('computed') ? '#FF5000' : (feature.get('source') ? '#00EDFF' : "#FF9900"),
+      opacity: laneDefault.fillOpacity
+    }),
+    text: new ol.style.Text({
+      text: feature.getGeometry().getType() === "LineString" ? '' : feature.get('laneNumber') ? 
+        (parseInt(feature.get('laneNumber'), 10) < 10 ? parseInt(feature.get('laneNumber'), 10).toString() : feature.get('laneNumber').toString()) : '',
+      font: `${laneDefault.fontSize} ${laneDefault.fontFamily}`,
+      fill: new ol.style.Fill({
+        color: '#000'
+      })
+    }),
+    image: new ol.style.Circle({
+      radius: laneDefault.pointRadius,
+      fill: new ol.style.Fill({
+        color: feature.get('computed') ? '#FF5000' : (feature.get('source') ? '#00EDFF' : laneDefault.fillColor)
+      })
+    })
+  });
+};
+
+const measureStyle = (feature) => {
+  return new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: feature.get('computed') ? '#FF5000' : (feature.get('source') ? '#00EDFF' : "#FF9900"),
+      width: measureDefault.strokeWidth,
+      opacity: measureDefault.strokeOpacity
+    }),
+    fill: new ol.style.Fill({
+      color: feature.get('computed') ? '#FF5000' : (feature.get('source') ? '#00EDFF' : "#FF9900"),
+      opacity: laneDefault.fillOpacity
+    })
+  });
+};
+
+const barStyle = (feature) => {
+  return new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0)', 
+    }),
+    stroke: new ol.style.Stroke({
+      color: barDefault.strokeColor,
+      width: barDefault.strokeWidth,
+      opacity: barDefault.strokeOpacity
+    })
+  });
+};
+
+const barHighlightedStyle = ()=>{
+  return new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(61, 65, 176, 0.5)',
+    }),
+    stroke: new ol.style.Stroke({
+      color: 'rgb(6, 14, 226)',
+      width: 3,
+    })
+  });
+}
+
+
+const vectorStyle = (feature)=>{
+  console.log(feature);
+  return new ol.style.Style(vectorDefault)
+};
+
+const widthStyle = (feature) => {
+  return new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: `rgba(255, 255, 0, ${widthDefault.strokeOpacity})`, 
+      width: widthDefault.strokeWidth
+    }),
+    fill: new ol.style.Fill({
+      color: `rgba(255, 255, 0, ${widthDefault.fillOpacity})`
+    }),
+    image: new ol.style.Circle({
+      radius: widthDefault.pointRadius,
+      fill: new ol.style.Fill({
+        color: `rgba(255, 255, 0, ${widthDefault.fillOpacity})`
+      }),
+      stroke: new ol.style.Stroke({
+        color: `rgba(255, 255, 0, ${widthDefault.strokeOpacity})`, 
+        width: widthDefault.strokeWidth
+      })
+    })
+  });
+};
+
+const connectionsStyle = (feature) => {
+  return new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: connectionsDefault.strokeColor,
+      width: connectionsDefault.strokeWidth,
+      opacity: connectionsDefault.strokeOpacity
+    }),
+    fill: new ol.style.Fill({
+      color: connectionsDefault.fillColor,
+      opacity: connectionsDefault.fillOpacity
+    }),
+    image: new ol.style.RegularShape({
+      points: 3,
+      radius: connectionsDefault.pointRadius,
+      angle: feature.get('angle') || 0,
+      fill: new ol.style.Fill({
+        color: connectionsDefault.fillColor
+      })
+    })
+  });
+};
+
+
+const lineStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'red',
+    width: 2
+  })
+});
+
+const pointStyle = new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 5,
+    fill: new ol.style.Fill({
+      color: 'red'
+    })
+  })
+});
+
+const errorMarkerStyle = new ol.style.Style({
+  image: new ol.style.Icon({
+    src: 'img/error.png',
+    size: [21, 25],
+    anchor: [0.5, 1],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'fraction'
+  })
+});
+
+// Style for midpoints (less opacity)
+const midMarkerStyle = new ol.style.Style({
+  image: new ol.style.Circle({
+      radius: 5,
+      fill: new ol.style.Fill({ color: 'rgba(240, 133, 45, 0.5)' }), // Semi-transparent
+      stroke: new ol.style.Stroke({ color: 'rgba(247, 154, 14, 0.5)', width: 1 })
+  })
+});
+
+
+export {
+  laneStyle,
+  vectorStyle,
+  lineStyle,
+  widthStyle,
+  connectionsStyle,
+  errorMarkerStyle,
+  barStyle,
+  pointStyle,
+  barHighlightedStyle,
+  measureStyle,
+  midMarkerStyle
+};
