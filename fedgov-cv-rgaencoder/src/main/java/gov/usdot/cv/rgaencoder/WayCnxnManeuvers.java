dbb0a8a5ed5f779108f6ba9b001fddfb7da23c93
Satish Natarajan <natarajan.sa@northeastern.edu>
2025-06-18 15:49:55 -0400

    Created JNI wrapper

--- fedgov-cv-rgaencoder/src/main/java/gov/usdot/cv/rgaencoder/WayCnxnManeuvers.java
+++ fedgov-cv-rgaencoder/src/main/java/gov/usdot/cv/rgaencoder/WayCnxnManeuvers.java
@@ -17,20 +17,39 @@
 package gov.usdot.cv.rgaencoder;
 
 public class WayCnxnManeuvers {
-    private enum wayCnxnManeuversOptions {
-        straight("straight"),
-        leftTurn("leftTurn"),
-        rightTurn("rightTurn"),
-        leftUTurn("leftUTurn"),
-        rightUTurn("rightUTurn");
 
-        private final String value;
-        wayCnxnManeuversOptions(String value) {
-            this.value = value;
-        }
-        public String getValue() {
-            return value;
-        }
-        
+    private long wayCnxnManeuversValue;
+
+    // Constants for choice field
+    public static final long STRAIGHT = 0;
+    public static final long LEFT_TURN = 1;
+    public static final long RIGHT_TURN = 2;
+    public static final long LEFT_U_TURN = 3;
+    public static final long RIGHT_U_TURN = 4;
+
+    // Constructors
+    public WayCnxnManeuvers() {
     }
+
+    public WayCnxnManeuvers(long wayCnxnManeuversValue) {
+        this.wayCnxnManeuversValue = wayCnxnManeuversValue;
+    }
+
+    // Getters and Setters
+    public long getWayCnxnManeuvers() {
+        return wayCnxnManeuversValue;
+    }
+
+    public void setWayCnxnManeuvers(long wayCnxnManeuversValue) {
+        this.wayCnxnManeuversValue = wayCnxnManeuversValue;
+    }
+
+    @Override
+    public String toString() {
+        return "WayCnxnManeuvers{" +
+                "wayCnxnManeuversValue=" + wayCnxnManeuversValue +
+                '}';
+    }
+
+
 }
