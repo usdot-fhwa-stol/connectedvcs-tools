f8dd380057ed739030230f893d91155bc560fe72
ChrisC027 <139565943+ChrisC027@users.noreply.github.com>
2025-07-07 17:32:05 -0400

    Map 304 connections time restrictions (#149)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    - Added time restrictions to the connections configuration panel.
    - Added time restrictions to the JSON in the encoder. 
    - Configured RGA functionality to the time restrictions in UI and
    encoder.
    - Updated the UI of the configuration panel. Increased the width to
    accommodate all elements in the connections table.
    <!--- Describe your changes in detail -->
    
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    MAP-304
    <!-- e.g. CAR-123 -->
    
    ## Motivation and Context
    To allow users to seamlessly create MAP and RGA messages with the tool,
    you will implement the following within the tool’s Lane Info Connections
    dialog:
    - Add time restriction selectors to the Lane Info Connections dialog for
    each row of the table
    - If RGA is disabled, disable any time restriction fields.
    
    Ensure that the time restriction information appears in the message
    encoder. This time restriction should be set for the connection and the
    connection maneuver.
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    Local Docker deployment testing.
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

--- fedgov-cv-ISDcreator-webapp/src/main/webapp/js/map.js
+++ fedgov-cv-ISDcreator-webapp/src/main/webapp/js/map.js
@@ -1,4 +1,4 @@
-import {addLaneInfoTimeRestrictions, addApproachTimeRestrictions, addRow, addApproachRow, isSpeedLimitTypePassengerVehicleMaxSpeedSelected, isSpeedLimitTypePassengerVehicleMinSpeedSelected, deleteRow, deleteApproachRow, getCookie, getLaneInfoDaySelection, getLaneInfoTimePeriod, hideRGAFields, hideRGAFieldsAssociatedToSpeedLimits, updateDeleteButtonStates, makeDroppable, onMappedGeomIdChangeCallback, onRegionIdChangeCallback, onRoadAuthorityIdChangeCallback, rebuildConnections, rebuildApproaches, removeSpeedForm, resetRGAStatus, resetSpeedDropdowns, saveApproaches, saveConnections, saveSpeedForm, setLaneAttributes, setRGAStatus, toggle, toggleBars, toggleLanes, toggleLaneTypeAttributes, togglePoints, toggleWidthArray, unselectFeature, updateSharedWith, updateTimeRestrictionsHTML, updateApproachTimeRestrictionsHTML, updateTypeAttributes} from "./utils.js";
+import {addLaneInfoTimeRestrictions, addApproachTimeRestrictions, addConnectionsTimeRestrictions, addRow, addApproachRow, isSpeedLimitTypePassengerVehicleMaxSpeedSelected, isSpeedLimitTypePassengerVehicleMinSpeedSelected, deleteRow, deleteApproachRow, getCookie, getLaneInfoDaySelection, getLaneInfoTimePeriod, hideRGAFields, hideRGAFieldsAssociatedToSpeedLimits, updateDeleteButtonStates, makeDroppable, onMappedGeomIdChangeCallback, onRegionIdChangeCallback, onRoadAuthorityIdChangeCallback, rebuildConnections, rebuildApproaches, removeSpeedForm, resetRGAStatus, resetSpeedDropdowns, saveApproaches, saveConnections, saveSpeedForm, setLaneAttributes, setRGAStatus, toggle, toggleBars, toggleLanes, toggleLaneTypeAttributes, togglePoints, toggleWidthArray, unselectFeature, updateSharedWith, updateTimeRestrictionsHTML, updateApproachTimeRestrictionsHTML, updateConnectionsTimeRestrictionsHTML, updateTypeAttributes} from "./utils.js";
 import {newChildMap, newParentMap, openChildMap, openParentMap, selected, updateChildParent}  from "./parent-child-latest.js"
 import {deleteTrace, loadKMLTrace, loadRSMTrace, saveMap, toggleControlsOn,} from "./files.js";
 import {barHighlightedStyle, barStyle, connectionsStyle, errorMarkerStyle, laneStyle, measureStyle, pointStyle, vectorStyle, widthStyle} from "./style.js";
@@ -52,6 +52,7 @@ let temporaryLaneMarkers;
 let temporaryBoxMarkers;
 let timeRestrictions;
 let approachTimeRestrictions;
+let connectionsTimeRestrictions;
 
 function initMap() {
   const baseAerialLayer = new ol.layer.Tile({
@@ -1127,6 +1128,12 @@ function initMISC() {
     approachTimeRestrictions = data;
     addApproachTimeRestrictions(approachTimeRestrictions);
     updateApproachTimeRestrictionsHTML();
+  })
+
+  $.get("js/time-restrictions.html", function (data) {
+    connectionsTimeRestrictions = data;
+    addConnectionsTimeRestrictions(connectionsTimeRestrictions);
+    updateConnectionsTimeRestrictionsHTML();
   })
 }
 
