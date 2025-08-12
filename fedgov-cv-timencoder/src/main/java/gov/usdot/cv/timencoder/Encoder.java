07c6491af633c486b5baff3b10fd3c00e6b05050
Mahbubul Alam Palash <palashm@leidos.com>
2025-08-12 16:08:05 -0400

    CVCS-42 Created packages and skeleton code for TIM Tool compiler Update

---
+++ fedgov-cv-timencoder/src/main/java/gov/usdot/cv/timencoder/Encoder.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2025 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package gov.usdot.cv.timencoder;
+import java.nio.ByteOrder;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Encoder {
+    private static final Logger logger = LogManager.getLogger(Encoder.class);
+
+    public Encoder() {
+    }
+
+    // Load native library for TIM encoding
+    static {
+        try {
+            System.loadLibrary("asn1c_timencoder");
+        } catch (Exception e) {
+            logger.error("Exception while loading the asn1c_timencoder library: " + e.toString(), e);
+        }
+    }
+
+    /**
+     * Native method that encodes a TIM message using the ASN.1 C library.
+     * 
+     * @return encoded TIM message as byte array, or null if encoding fails
+     */
+    public native byte[] encodeTIM();
+
+    public ByteArrayObject encode(TIMData message) {
+        System.out.println("Calling Encoder Java API");
+		logger.debug("Starting TIM encoding process...");
+
+        byte[] encodeMsg = encodeTIM();
+
+        if (encodeMsg == null) {
+            logger.error("Encoding failed. Returned byte array is null.");
+            return new ByteArrayObject("TIM", null);
+        }
+
+        ChannelBuffer buffer = ChannelBuffers.copiedBuffer(ByteOrder.LITTLE_ENDIAN, encodeMsg);
+        byte[] byteArray = new byte[buffer.readableBytes()];
+        buffer.readBytes(byteArray);
+        
+        String hexString = Hex.encodeHexString(byteArray);
+        logger.debug("Encoded hex string of the TIM message: " + hexString);
+
+        return new ByteArrayObject("TIM", byteArray);
+    }
+}
---
+++ fedgov-cv-timencoder/src/test/java/gov/usdot/cv/timencoder/TIMEncodeTest.java
@@ -0,0 +1,44 @@
+
+/*
+ * Copyright (C) 2025 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package gov.usdot.cv.timencoder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TIMEncodeTest {
+    private static final Logger logger = LogManager.getLogger(TIMEncodeTest.class);
+    Encoder encoder;
+    TIMData mockTimData;
+    @Before
+    public void setUp() {
+         encoder = new Encoder(); 
+
+        }
+
+    @Test
+    public void TIM_encode_test() {
+    ByteArrayObject result = encoder.encode(mockTimData);
+    Assert.assertNotNull("Byte array should not be null", result.getMessage());    
+    byte[] expected = new byte[] { (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF };
+    Assert.assertArrayEquals("Encoded TIM message doesn't match expected output", expected, result.getMessage());
+
+    }
+}
