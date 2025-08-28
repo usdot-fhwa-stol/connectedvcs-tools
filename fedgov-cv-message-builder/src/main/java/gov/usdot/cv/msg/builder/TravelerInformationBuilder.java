be91b7b3599eeb3c5fd04d2bb0d11866690591ef
Mahbubul Alam Palash <palashm@leidos.com>
2025-08-28 13:17:54 -0400

    CVCS-44 update Message builder for TIM

--- fedgov-cv-message-builder/src/main/java/gov/usdot/cv/msg/builder/TravelerInformationBuilder.java
+++ fedgov-cv-message-builder/src/main/java/gov/usdot/cv/msg/builder/TravelerInformationBuilder.java
@@ -14,6 +14,8 @@
  * the License.
  */
 package gov.usdot.cv.msg.builder;
+import java.nio.ByteBuffer;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -25,6 +27,13 @@ import org.apache.logging.log4j.Logger;
 
 import gov.usdot.cv.msg.builder.exception.MessageBuildException;
 import gov.usdot.cv.msg.builder.input.IntersectionInputData;
+import gov.usdot.cv.timencoder.MinuteOfTheYear;
+import gov.usdot.cv.timencoder.MinutesDuration;
+import gov.usdot.cv.timencoder.SSPindex;
+import gov.usdot.cv.timencoder.TravelerDataFrame;
+import gov.usdot.cv.timencoder.TravelerDataFrameList;
+import gov.usdot.cv.timencoder.TravelerInfoType;
+import gov.usdot.cv.timencoder.TravelerInformation;
 import gov.usdot.cv.msg.builder.input.TravelerInputData;
 import gov.usdot.cv.msg.builder.message.SemiMessage;
 import gov.usdot.cv.msg.builder.message.TravelerInformationMessage;
@@ -41,11 +50,36 @@ public class TravelerInformationBuilder {
 	@Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.TEXT_PLAIN)
 	public TravelerInformationMessage build(String timData) {
+        logger.debug("Building TIM/ADV with input data : " + timData);
 		TravelerInformationMessage tim = new TravelerInformationMessage();
         TravelerInputData travInputData = null;
+		boolean deposit = false;
+		SemiMessage semi = null;
+		TravelerInformation ti = null;
+        /* 
+        MessageFrame mf = null;
+		GenerateType generateType = GenerateType.TIM;
+        */
+        travInputData.applyLatLonOffset();
+		travInputData.initialzeReferencePoints();
+		//generateType = travInputData.getGenerateType();
+		//ti = buildTravelerInformation(travInputData);
 
         try {
 			travInputData = JSONMapper.jsonStringToPojo(timData, TravelerInputData.class);
+            deposit = (travInputData != null && travInputData.deposit != null);
+           
+            //TODO:
+            //validate is commented out will investigate later
+            //travInputData.validate();
+            travInputData.applyLatLonOffset();
+			travInputData.initialzeReferencePoints();
+			//generateType = travInputData.getGenerateType();
+
+			ti = buildTravelerInformation(travInputData);
+
+
+            
 
 		} catch (Exception e) {
 			logger.error("Error parsing TravelerInputData ", e);
@@ -56,4 +90,42 @@ public class TravelerInformationBuilder {
         return tim;
 
     }
+
+    private TravelerInformation buildTravelerInformation(TravelerInputData travInputData) {
+		TravelerInformation tim = new TravelerInformation();
+		tim.setDataFrames(buildDataFrames(travInputData));
+		//tim.setMsgCnt(tim.getDataFrames().getSize());
+        //Todo: will replace with method from tim Dataframe Size
+        tim.setMsgCnt(1000);
+		ByteBuffer buf = ByteBuffer.allocate(9).put((byte)0).putLong(travInputData.anchorPoint.packetID);
+		//Unique MsgID is optional and will implement in a later story
+       // tim.setPacketID(new UniqueMSGID(buf.array()));
+		return tim;
+	}
+
+    private TravelerDataFrameList buildDataFrames(TravelerInputData travInputData) {
+		TravelerDataFrameList dataFrames = new TravelerDataFrameList();
+		TravelerDataFrame dataFrame = new TravelerDataFrame();
+		
+		// -- Part I, Frame header
+		dataFrame.setDoNotUse1(new SSPindex(travInputData.anchorPoint.sspTimRights));
+		dataFrame.setFrameType(new TravelerInfoType());
+		dataFrame.setMsgId(getMessageId(travInputData));
+		dataFrame.setStartYear(new DYear(getStartYear(travInputData)));
+		dataFrame.setStartTime(new MinuteOfTheYear(getStartTime(travInputData)));
+		dataFrame.setDuratonTime(new MinutesDuration(getDurationTime(travInputData)));
+		dataFrame.setPriority(new SignPrority(travInputData.anchorPoint.priority));
+		
+		// -- Part II, Applicable Regions of Use
+		dataFrame.setSspLocationRights(new SSPindex(travInputData.anchorPoint.sspLocationRights));
+		dataFrame.setRegions(buildRegions(travInputData));
+		
+		// -- Part III, Content
+		dataFrame.setSspMsgRights1(new SSPindex(travInputData.anchorPoint.sspTypeRights));		// allowed message types
+		dataFrame.setSspMsgRights2(new SSPindex(travInputData.anchorPoint.sspContentRights));	// allowed message content	
+		dataFrame.setContent(buildContent(travInputData));
+		
+		dataFrames.add(dataFrame);
+		return dataFrames;
+	}
 }
