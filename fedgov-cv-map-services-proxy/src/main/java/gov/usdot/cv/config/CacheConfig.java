f7af02bef863af6ce6729783c2cad9f576ef650c
dan-du-car <dan.du@leidos.com>
2025-09-02 10:31:54 -0400

    resolve unit test

--- fedgov-cv-map-services-proxy/src/main/java/gov/usdot/cv/config/CacheConfig.java
+++ fedgov-cv-map-services-proxy/src/main/java/gov/usdot/cv/config/CacheConfig.java
@@ -15,7 +15,6 @@
  */
 package gov.usdot.cv.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
