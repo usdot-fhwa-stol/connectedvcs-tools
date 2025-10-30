eb4e52d0d7015fca3ff3d6c1effdf488b7e7a15c
Satish Natarajan <76594749+radishco@users.noreply.github.com>
2025-10-30 11:01:21 -0400

    Cvcs 68/69 road surface UI (#201)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    
    Adds both Road Condition fields (DescriptionofRoadSurface and
    RoadSurfaceCondition) in a new tab to the markers in the UI
    
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    
    
    [CVCS-68](https://usdot-carma.atlassian.net/jira/software/c/projects/CS/boards/34?selectedIssue=CVCS-68)
    [CVCS-69
    
    ](https://usdot-carma.atlassian.net/jira/software/c/projects/CS/boards/34?selectedIssue=CVCS-69)
    
    ## Motivation and Context
    
    Needed to add UI changes for Road Condition Fields
    
    ## How Has This Been Tested?
    
    Local Deployment Testing
    
    <img width="625" height="286" alt="image"
    src="https://github.com/user-attachments/assets/fa9bedb7-5afc-4a50-ac02-0733926b7960"
    />
    
    <img width="818" height="518" alt="image"
    src="https://github.com/user-attachments/assets/d605fa20-a3ff-45c1-a20f-9e15c3994110"
    />
    
    Missing road condition field:
    <img width="885" height="575" alt="image"
    src="https://github.com/user-attachments/assets/991a8270-2c10-470f-92c2-39bcda718e7a"
    />
    
    
    
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [ x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x ] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    
    [CVCS-68]:
    https://usdot-carma.atlassian.net/browse/CVCS-68?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- fedgov-cv-TIMcreator-webapp/src/main/webapp/js/depositor.js
+++ fedgov-cv-TIMcreator-webapp/src/main/webapp/js/depositor.js
@@ -327,7 +327,9 @@ function createMessageJSON() {
                 priority: attrs.priority,
                 startTime: attrs.startTime,
                 endTime: attrs.endTime,
-                heading: getHeading(attrs.heading)
+                heading: getHeading(attrs.heading),
+                road_surface: attrs.road_surface?.substring(1, 2),
+                road_condition: attrs.road_condition
             };
         }
 
@@ -411,6 +413,7 @@ function errorCheck() {
                 const content = feature.get('content');
                 const priority = feature.get('priority');
                 const mutcd = feature.get('mutcd');
+                const road_condition = feature.get('road_condition');
 
                 if (!startTime || !endTime) {
                     $('#alert_placeholder').html(`<div class="alert alert-danger alert-dismissable">
@@ -440,6 +443,14 @@ function errorCheck() {
                     $('#alert_placeholder').html(`<div class="alert alert-danger alert-dismissable">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                         <span>Missing mutcd codes.</span>
+                    </div>`);
+                    status = true;
+                }
+
+                if (!road_condition) {
+                    $('#alert_placeholder').html(`<div class="alert alert-danger alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <span>Missing road condition.</span>
                     </div>`);
                     status = true;
                 }
