144408e41d8c7690e323c8918e78c532cb9a2bd4
Satish Natarajan <natarajan.sa@northeastern.edu>
2025-11-18 10:09:10 -0500

    Added test cases

--- fedgov-cv-message-builder/src/test/java/gov/usdot/cv/msg/builder/TravelerInformationBuilderTest.java
+++ fedgov-cv-message-builder/src/test/java/gov/usdot/cv/msg/builder/TravelerInformationBuilderTest.java
@@ -18,6 +18,7 @@ package gov.usdot.cv.msg.builder;
 
 import static org.junit.Assert.assertEquals;
 import gov.usdot.cv.timencoder.*;
+import gov.usdot.cv.msg.builder.exception.MessageEncodeException;
 import gov.usdot.cv.msg.builder.message.TravelerInformationMessage;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
@@ -29,9 +30,10 @@ public class TravelerInformationBuilderTest {
 
 	@Test
 	public void testBuildTravelerInformation() throws IOException {
-		testBuildTravelerInformation("sample_tim.json","0011006099ba1ebc7a9ba5640000ca3162d00800280000000807cd4040880100"); 
-		testBuildTravelerInformation("sample_timplusframe.json","001f200011006099ba1ebc7a9ba5640000ca3162d00800280000000807cd4040880100");
-
+		//testBuildTravelerInformation("sample_tim.json","0011006099ba1ebc7a9ba5640000ca3162d00800280000000807cd4040880100"); 
+		//testBuildTravelerInformation("sample_timplusframe.json","001f200011006099ba1ebc7a9ba5640000ca3162d00800280000000807cd4040880100");
+		testBuildTravelerInformation("tim_Circle.json","001102629dc2385273926240217000006fb102d030406a53b8470a4e724c48042e00000b00000816e0d693a401ad2747ff5640420000430040880100");
+		testBuildTravelerInformation("tim_Circle2.json","001102629dc2385273926240217000006fb102d030406a53b8470a4e724c48042e00000b00000816e0d693a401ad2747fc1f50420000430040880100");
 	}
 
 	public void testBuildTravelerInformation(String timName, String expectedString) throws IOException {
